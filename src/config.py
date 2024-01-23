3704adad7e6b2fc933d9af268ac40a9294d2f5e9
Margarida Vieira <margarida.j.vieira@inesctec.pt>
2024-01-23 09:57:40 +0000

    fix pornography-2k data dir path so it would be more general

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
 DATA_DIR = "/nas-ctm01/datasets/public/BIOMETRICS"
 PORN_800_DIR = f"{DATA_DIR}/pornography-database/data"
-PORN_2K_DIR = f"{DATA_DIR}/pornography-2k-db/data/original"
+PORN_2K_DIR = f"{DATA_DIR}/pornography-2k-db/data"
