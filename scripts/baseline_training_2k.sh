ab558ce97dbd0667110561d04ae497904c7347e4
Margarida Vieira <margaridajcv.27@gmail.com>
2024-02-21 09:16:45 +0000

    change partition in training scripts

--- scripts/baseline_training_2k.sh
+++ scripts/baseline_training_2k.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --partition=gpu_min12GB         # Partition where the job will be run. Check with "$ sinfo".
-#SBATCH --qos=gpu_min12GB_ext           # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
+#SBATCH --partition=gpu_min11GB         # Partition where the job will be run. Check with "$ sinfo".
+#SBATCH --qos=gpu_min11GB_ext           # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
 #SBATCH --job-name=baseline_training_2k # Job name
 #SBATCH -o slurm.%N.%j.out              # File containing STDOUT output
 #SBATCH -e slurm.%N.%j.err              # File containing STDERR output. If ommited, use STDOUT.
