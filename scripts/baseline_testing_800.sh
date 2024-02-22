37753922130649145b3475468ecd0c030e8b7a68
Margarida Vieira <margaridajcv.27@gmail.com>
2024-02-22 10:20:06 +0000

    fix scripts according to new repo structure

--- scripts/baseline_testing_800.sh
+++ scripts/baseline_testing_800.sh
@@ -6,73 +6,73 @@
 #SBATCH -o slurm.%N.%j.out          # File containing STDOUT output
 #SBATCH -e slurm.%N.%j.err          # File containing STDERR output. If ommited, use STDOUT.
 
-echo "Running baseline testing job on Pornography-800\n"
+echo "Running baseline testing job on Pornography-800"
 
-echo "Testing ResNet50\n"
-python src/baseline/baseline_testing.py \
+echo "Testing ResNet50"
+python -m src.model_testing.py \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --save_loc "baseline/pornography-800/results" \
        --state_dict_loc "baseline/pornography-800/models/model_resnet50_freeze_True_epochs_10_batch_32_optim_sgd_optimized_False.pth" \
        --model_name "resnet50"
 
-echo "Testing ResNet101\n"
-python src/baseline/baseline_testing.py \
+echo "Testing ResNet101"
+python -m src.model_testing.py \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --save_loc "baseline/pornography-800/results" \
        --state_dict_loc "baseline/pornography-800/models/model_resnet101_freeze_True_epochs_10_batch_32_optim_sgd_optimized_False.pth" \
        --model_name "resnet101"
 
-echo "Testing ResNet152\n"
-python src/baseline/baseline_testing.py \
+echo "Testing ResNet152"
+python -m src.model_testing.py \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --save_loc "baseline/pornography-800/results" \
        --state_dict_loc "baseline/pornography-800/models/model_resnet152_freeze_True_epochs_10_batch_32_optim_sgd_optimized_False.pth" \
        --model_name "resnet152"
 
-echo "Testing DenseNet121\n"
-python src/baseline/baseline_testing.py \
+echo "Testing DenseNet121"
+python -m src.model_testing.py \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --save_loc "baseline/pornography-800/results" \
        --state_dict_loc "baseline/pornography-800/models/model_densenet121_freeze_True_epochs_10_batch_32_optim_sgd_optimized_False.pth" \
        --model_name "densenet121"
 
-echo "Testing DenseNet169\n"
-python src/baseline/baseline_testing.py \
+echo "Testing DenseNet169"
+python -m src.model_testing.py \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --save_loc "baseline/pornography-800/results" \
        --state_dict_loc "baseline/pornography-800/models/model_densenet169_freeze_True_epochs_10_batch_32_optim_sgd_optimized_False.pth" \
        --model_name "densenet169"
 
-echo "Testing DenseNet201\n"
-python src/baseline/baseline_testing.py \
+echo "Testing DenseNet201"
+python -m src.model_testing.py \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --save_loc "baseline/pornography-800/results" \
        --state_dict_loc "baseline/pornography-800/models/model_densenet201_freeze_True_epochs_10_batch_32_optim_sgd_optimized_False.pth" \
        --model_name "densenet201"
 
-echo "Testing AlexNet\n"
-python src/baseline/baseline_testing.py \
+echo "Testing AlexNet"
+python -m src.model_testing.py \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --save_loc "baseline/pornography-800/results" \
        --state_dict_loc "baseline/pornography-800/models/model_alexnet_freeze_True_epochs_10_batch_32_optim_sgd_optimized_False.pth" \
        --model_name "alexnet"
 
-echo "Testing VGG16\n"
-python src/baseline/baseline_testing.py \
+echo "Testing VGG16"
+python -m src.model_testing.py \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --save_loc "baseline/pornography-800/results" \
        --state_dict_loc "baseline/pornography-800/models/model_vgg16_freeze_True_epochs_10_batch_32_optim_sgd_optimized_False.pth" \
        --model_name "vgg16"
 
-echo "Testing VGG19\n"
-python src/baseline/baseline_testing.py \
+echo "Testing VGG19"
+python -m src.model_testing.py \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --save_loc "baseline/pornography-800/results" \
        --state_dict_loc "baseline/pornography-800/models/model_vgg19_freeze_True_epochs_10_batch_32_optim_sgd_optimized_False.pth" \
        --model_name "vgg19"
 
-echo "Testing MobileNetV2\n"
-python src/baseline/baseline_testing.py \
+echo "Testing MobileNetV2"
+python -m src.model_testing.py \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --save_loc "baseline/pornography-800/results" \
        --state_dict_loc "baseline/pornography-800/models/model_mobilenetv2_freeze_True_epochs_10_batch_32_optim_sgd_optimized_False.pth" \
--- scripts/baseline_training_800.sh
+++ scripts/baseline_training_800.sh
@@ -6,75 +6,84 @@
 #SBATCH -o slurm.%N.%j.out               # File containing STDOUT output
 #SBATCH -e slurm.%N.%j.err               # File containing STDERR output. If ommited, use STDOUT.
 
-echo "Running baseline training job on Pornography-800\n"
+echo "Running baseline training job on Pornography-800"
 
-echo "Training ResNet50\n"
-python src/baseline/baseline_training.py \
+echo "Training ResNet50"
+python -m src.model_training \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --model_save_loc "baseline/pornography-800/models" \
        --metrics_save_loc "baseline/pornography-800/metrics" \
-       --model_name "resnet50"
+       --model_name "resnet50" \
+       --freeze_layers
 
-echo "Training ResNet101\n"
-python src/baseline/baseline_training.py \
+echo "Training ResNet101"
+python -m src.model_training \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --model_save_loc "baseline/pornography-800/models" \
        --metrics_save_loc "baseline/pornography-800/metrics" \
-       --model_name "resnet101"
+       --model_name "resnet101" \
+       --freeze_layers
 
-echo "Training ResNet152\n"
-python src/baseline/baseline_training.py \
+echo "Training ResNet152"
+python -m src.model_training \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --model_save_loc "baseline/pornography-800/models" \
        --metrics_save_loc "baseline/pornography-800/metrics" \
-       --model_name "resnet152"
+       --model_name "resnet152" \
+       --freeze_layers
 
-echo "Training DenseNet121\n"
-python src/baseline/baseline_training.py \
+echo "Training DenseNet121"
+python -m src.model_training \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --model_save_loc "baseline/pornography-800/models" \
        --metrics_save_loc "baseline/pornography-800/metrics" \
-       --model_name "densenet121"
+       --model_name "densenet121" \
+       --freeze_layers
 
-echo "Training DenseNet169\n"
-python src/baseline/baseline_training.py \
+echo "Training DenseNet169"
+python -m src.model_training \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --model_save_loc "baseline/pornography-800/models" \
        --metrics_save_loc "baseline/pornography-800/metrics" \
-       --model_name "densenet169"
+       --model_name "densenet169" \
+       --freeze_layers
 
-echo "Training DenseNet201\n"
-python src/baseline/baseline_training.py \
+echo "Training DenseNet201"
+python -m src.model_training \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --model_save_loc "baseline/pornography-800/models" \
        --metrics_save_loc "baseline/pornography-800/metrics" \
-       --model_name "densenet201"
+       --model_name "densenet201" \
+       --freeze_layers
 
-echo "Training AlexNet\n"
-python src/baseline/baseline_training.py \
+echo "Training AlexNet"
+python -m src.model_training \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --model_save_loc "baseline/pornography-800/models" \
        --metrics_save_loc "baseline/pornography-800/metrics" \
-       --model_name "alexnet"
+       --model_name "alexnet" \
+       --freeze_layers
 
-echo "Training VGG16\n"
-python src/baseline/baseline_training.py \
+echo "Training VGG16"
+python -m src.model_training \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --model_save_loc "baseline/pornography-800/models" \
        --metrics_save_loc "baseline/pornography-800/metrics" \
-       --model_name "vgg16"
+       --model_name "vgg16" \
+       --freeze_layers
 
-echo "Training VGG19\n"
-python src/baseline/baseline_training.py \
+echo "Training VGG19"
+python -m src.model_training \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --model_save_loc "baseline/pornography-800/models" \
        --metrics_save_loc "baseline/pornography-800/metrics" \
-       --model_name "vgg19"
+       --model_name "vgg19" \
+       --freeze_layers
 
-echo "Training MobileNetV2\n"
-python src/baseline/baseline_training.py \
+echo "Training MobileNetV2"
+python -m src.model_training \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-40" \
        --model_save_loc "baseline/pornography-800/models" \
        --metrics_save_loc "baseline/pornography-800/metrics" \
-       --model_name "mobilenetv2"
-
+       --model_name "mobilenetv2" \
+       --freeze_layers
