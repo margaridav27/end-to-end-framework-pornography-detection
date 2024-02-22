a0fb032cfeed7cef1b265d9275608da00ec0e9ea
Margarida Vieira <margaridajcv.27@gmail.com>
2024-02-22 11:00:56 +0000

    fix scripts name

--- scripts/baseline/training_2k.sh
+++ scripts/baseline/training_2k.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-#SBATCH --partition=gpu_min11GB         # Partition where the job will be run. Check with "$ sinfo".
-#SBATCH --qos=gpu_min11GB_ext           # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
-#SBATCH --job-name=baseline_training_2k # Job name
-#SBATCH -o slurm.%N.%j.out              # File containing STDOUT output
-#SBATCH -e slurm.%N.%j.err              # File containing STDERR output. If ommited, use STDOUT.
+#SBATCH --partition=gpu_min11GB         
+#SBATCH --qos=gpu_min11GB_ext            
+#SBATCH --job-name=baseline_training_2k
+#SBATCH -o slurm.%N.%j.out               
+#SBATCH -e slurm.%N.%j.err 
 
 echo "Running baseline training job on Pornography-2k"
 
