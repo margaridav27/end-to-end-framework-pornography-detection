5a7058e405d90c3236af5633743a192dbf10944a
Margarida Vieira <margaridajcv.27@gmail.com>
2024-03-08 16:29:48 +0000

    new frame extraction strategy

--- src/frame_extraction/frame_extraction.py
+++ src/frame_extraction/frame_extraction.py
@@ -1,18 +1,27 @@
 import os
 import argparse
 
-from src.frame_extraction.frame_extractors import MiddleFrameExtractor
+from src.frame_extraction.frame_extractors import FrameExtractor, MiddleFrameExtractor, EvenFrameExtractor
 
 parser = argparse.ArgumentParser(description="Extracting frames from dataset")
 
 parser.add_argument("--data_loc", required=True, nargs="*", type=str, help="Path of folder(s) that contain the videos from which frames are to be extracted")
 parser.add_argument("--save_loc", required=True, type=str, help="Path of the folder where the extracted frames are to be saved")
 parser.add_argument("--n_frames", required=True, type=int, help="Number of frames to extract from each video")
+parser.add_argument("--strat", type=str, default="middle", help="Strategy to use in the extraction (either middle or even)")
+parser.add_argument("--perc", type=float, default=0.8, help="Percentage of video to extract frames from (only if strat=even)")
 
 args = parser.parse_args()
 
 if not os.path.exists(args.save_loc):
     os.makedirs(args.save_loc)
 
-extractor = MiddleFrameExtractor(args.data_loc, args.save_loc, args.n_frames)
+extractor = FrameExtractor()
+if args.strat == "middle":
+    extractor = MiddleFrameExtractor(args.data_loc, args.save_loc, args.n_frames)
+elif args.strat == "even":
+    extractor = EvenFrameExtractor(args.data_loc, args.save_loc, args.n_frames, args.perc)
+else:
+    raise ValueError("Invalid --strat argument")
+
 extractor()
--- src/frame_extraction/frame_extractors.py
+++ src/frame_extraction/frame_extractors.py
@@ -69,3 +69,72 @@ class MiddleFrameExtractor(FrameExtractor):
         self._close_video(video)
     
     pd.DataFrame(data).to_csv(f"{self.save_loc}/data.csv", index=False)
+
+
+class EvenFrameExtractor(FrameExtractor):
+  def __init__(self, data_loc : list, save_loc : str, n_frames : int, perc : float):
+    super().__init__()
+
+    for loc in data_loc:
+      assert os.path.exists(loc) and os.path.isdir(loc), "Invalid --data_loc argument."
+    assert os.path.exists(save_loc) and os.path.isdir(save_loc), "Invalid --save_loc argument."
+
+    self.data_loc = data_loc
+    self.save_loc = save_loc
+    self.n_frames = n_frames
+    self.perc = perc
+
+  def __call__(self):
+    data = { "frame": [], "label": [] }
+
+    for loc in self.data_loc:
+      for v in os.listdir(loc):
+        if v.startswith("."): continue
+
+        video_path = os.path.join(loc, v)
+        video = self._open_video(video_path)
+        
+        frame_count = self._get_frame_count(video)
+        real_n_frames = self.n_frames
+  
+        if self.n_frames > frame_count:
+          real_n_frames = frame_count
+          print(f"Number of frames {self.n_frames} exceeds {frame_count}. "
+                f"Extracting {frame_count} frames instead")
+
+        ignore_frames = frame_count * (1. - self.perc) # Number of frames to be ignored
+        interval = (frame_count - 2 * ignore_frames) // real_n_frames # Interval at which frames are to be extracted
+        extracted_frames = 0
+        frame_i = 0
+
+        while True:
+          ret, frame = video.read()
+
+          # Advance to next frame
+          if not ret or frame_i < ignore_frames: 
+            frame_i += 1
+            continue
+          
+          # Although it is already in the portion to be ignored, 
+          # it only stops if it has already extracted all the frames 
+          # or is at the end of the video
+          if frame_i >= frame_count - ignore_frames:
+            if extracted_frames == real_n_frames or frame_i == frame_count - 1:
+              break
+
+          if frame_i % interval == 0:
+            frame_name = f"{v.split('.')[0]}#{extracted_frames}.jpg"
+            frame_label = 0 if "NonPorn" in v else 1
+
+            data["frame"].append(frame_name)
+            data["label"].append(frame_label)
+
+            cv.imwrite(f"{self.save_loc}/{frame_name}", frame)
+
+            extracted_frames += 1
+          
+          frame_i += 1
+
+        self._close_video(video)
+    
+    pd.DataFrame(data).to_csv(f"{self.save_loc}/data.csv", index=False)
