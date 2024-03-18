4a30e6e912221203578e6fd38026c525bbcd6ad3
Margarida Vieira <margaridajcv.27@gmail.com>
2024-03-18 15:51:32 +0000

    fix scripts paths

--- scripts/no_freeze/testing_middle_2k.sh
+++ scripts/no_freeze/testing_middle_2k.sh
@@ -12,58 +12,58 @@ echo "Testing ResNet50"
 python -m src.model_testing \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-2k-db/data-processed/middle-20" \
        --save_loc "results/middle-20/no-freeze/pornography-2k/results" \
-       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/model_resnet50_freeze_False_epochs_10_batch_32_optim_sgd.pth"
+       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/resnet50_freeze_False_epochs_50_batch_16_optim_sgd_aug_False_split_10_20.pth"
 
 echo "Testing ResNet101"
 python -m src.model_testing \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-2k-db/data-processed/middle-20" \
        --save_loc "results/middle-20/no-freeze/pornography-2k/results" \
-       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/model_resnet101_freeze_False_epochs_10_batch_32_optim_sgd.pth"
+       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/resnet101_freeze_False_epochs_50_batch_16_optim_sgd_aug_False_split_10_20.pth"
 
 echo "Testing ResNet152"
 python -m src.model_testing \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-2k-db/data-processed/middle-20" \
        --save_loc "results/middle-20/no-freeze/pornography-2k/results" \
-       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/model_resnet152_freeze_False_epochs_10_batch_32_optim_sgd.pth"
+       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/resnet152_freeze_False_epochs_50_batch_16_optim_sgd_aug_False_split_10_20.pth"
 
 echo "Testing DenseNet121"
 python -m src.model_testing \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-2k-db/data-processed/middle-20" \
        --save_loc "results/middle-20/no-freeze/pornography-2k/results" \
-       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/model_densenet121_freeze_False_epochs_10_batch_32_optim_sgd.pth"
+       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/densenet121_freeze_False_epochs_50_batch_16_optim_sgd_aug_False_split_10_20.pth"
 
 echo "Testing DenseNet169"
 python -m src.model_testing \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-2k-db/data-processed/middle-20" \
        --save_loc "results/middle-20/no-freeze/pornography-2k/results" \
-       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/model_densenet169_freeze_False_epochs_10_batch_32_optim_sgd.pth"
+       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/densenet169_freeze_False_epochs_50_batch_16_optim_sgd_aug_False_split_10_20.pth"
 
 echo "Testing DenseNet201"
 python -m src.model_testing \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-2k-db/data-processed/middle-20" \
        --save_loc "results/middle-20/no-freeze/pornography-2k/results" \
-       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/model_densenet201_freeze_False_epochs_10_batch_32_optim_sgd.pth"
+       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/densenet201_freeze_False_epochs_50_batch_16_optim_sgd_aug_False_split_10_20.pth"
 
 echo "Testing AlexNet"
 python -m src.model_testing \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-2k-db/data-processed/middle-20" \
        --save_loc "results/middle-20/no-freeze/pornography-2k/results" \
-       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/model_alexnet_freeze_False_epochs_10_batch_32_optim_sgd.pth"
+       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/alexnet_freeze_False_epochs_50_batch_16_optim_sgd_aug_False_split_10_20.pth"
 
 echo "Testing VGG16"
 python -m src.model_testing \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-2k-db/data-processed/middle-20" \
        --save_loc "results/middle-20/no-freeze/pornography-2k/results" \
-       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/model_vgg16_freeze_False_epochs_10_batch_32_optim_sgd.pth"
+       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/vgg16_freeze_False_epochs_50_batch_16_optim_sgd_aug_False_split_10_20.pth"
 
 echo "Testing VGG19"
 python -m src.model_testing \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-2k-db/data-processed/middle-20" \
        --save_loc "results/middle-20/no-freeze/pornography-2k/results" \
-       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/model_vgg19_freeze_False_epochs_10_batch_32_optim_sgd.pth"
+       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/vgg19_freeze_False_epochs_50_batch_16_optim_sgd_aug_False_split_10_20.pth"
 
 echo "Testing MobileNetV2"
 python -m src.model_testing \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-2k-db/data-processed/middle-20" \
        --save_loc "results/middle-20/no-freeze/pornography-2k/results" \
-       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/model_mobilenetv2_freeze_False_epochs_10_batch_32_optim_sgd.pth"
+       --state_dict_loc "results/middle-20/no-freeze/pornography-2k/models/mobilenetv2_freeze_False_epochs_50_batch_16_optim_sgd_aug_False_split_10_20.pth"
