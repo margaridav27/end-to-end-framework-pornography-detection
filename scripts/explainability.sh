fc6ab9119713a8b42bbac1d09916f1f03e4f2906
Margarida Vieira <margarida.j.vieira@inesctec.pt>
2024-04-03 12:42:54 +0000

    add vgg16_bn and vgg19_bn

--- scripts/explainability.sh
+++ scripts/explainability.sh
@@ -6,8 +6,26 @@
 #SBATCH -o explainability.out               
 #SBATCH -e explainability.err
 
+
+# python -m src.model_explainability \
+#        --state_dict_loc "results/even-20/no-freeze/pornography-2k/models/resnet50_freeze_False_epochs_50_batch_16_optim_sgd_aug_False_split_10_20.pth" \
+#        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-2k-db/data-processed/even-20" \
+#        --save_loc "results/even-20/no-freeze/pornography-2k/explanations" \
+#        --batch_size 4 \
+#        --method "OCC" \
+#        --attribute_kwargs '{ "sliding_window_shapes": (3,8,8), "strides": (3,4,4) }'
+
+# python -m src.model_explainability \
+#        --state_dict_loc "results/even-20/no-freeze/pornography-2k/models/resnet50_freeze_False_epochs_50_batch_16_optim_sgd_aug_False_split_10_20.pth" \
+#        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-2k-db/data-processed/even-20" \
+#        --save_loc "results/even-20/no-freeze/pornography-2k/explanations" \
+#        --batch_size 4 \
+#        --method "IG"
+
 python -m src.model_explainability \
-       --state_dict_loc "results/even-20/data-aug/pornography-2k/models/vgg19_freeze_False_epochs_50_batch_16_optim_sgd_aug_True_split_10_20.pth" \
+       --state_dict_loc "results/even-20/no-freeze/pornography-2k/models/resnet50_freeze_False_epochs_50_batch_16_optim_sgd_aug_False_split_10_20.pth" \
        --data_loc "/nas-ctm01/datasets/public/BIOMETRICS/pornography-2k-db/data-processed/even-20" \
-       --save_loc "results/even-20/data-aug/pornography-2k/explanations" \
-       --method "IG"
+       --save_loc "results/even-20/no-freeze/pornography-2k/explanations" \
+       --batch_size 4 \
+       --method "IG" \
+       --noise_tunnel
