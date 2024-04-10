b78f4b97bf22cc7800d858eeabe18e54f6a0c744
Margarida Vieira <margaridajcv.27@gmail.com>
2024-04-10 11:05:46 +0100

    fix cp command in setup_apd_db script

--- scripts/setup_apd_db.sh
+++ scripts/setup_apd_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --partition=cpu_14cores      # Partition where the job will be run. Check with "$ sinfo".
-#SBATCH --qos=cpu_14cores_ext        # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
+#SBATCH --partition=gpu_min8GB       # Partition where the job will be run. Check with "$ sinfo".
+#SBATCH --qos=gpu_min8GB_ext         # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
 #SBATCH --job-name=apd_db_setup      # Job name
 #SBATCH -o apd_db_setup.out          # File containing STDOUT output
 #SBATCH -e apd_db_setup.err          # File containing STDERR output. If ommited, use STDOUT.
@@ -16,13 +16,13 @@ dest_dir="/nas-ctm01/datasets/public/BIOMETRICS/apd-video-db/data-agg"
 mkdir -p "$dest_dir"
 
 # Copy images from porn directory
-cp -r "$source_dir/porn/"* "$dest_dir"
+find "$source_dir/porn/" -maxdepth 1 -type f -print0 | xargs -0 cp -t "$dest_dir"
 
 # Copy images from non-porn directories
-cp -r "$source_dir/nonPorn/COCO/"* "$dest_dir"
-cp -r "$source_dir/nonPorn/GVIS/"* "$dest_dir"
-cp -r "$source_dir/nonPorn/ILSVRC2012/"* "$dest_dir"
-cp -r "$source_dir/nonPorn/IMDB-WIKI/"* "$dest_dir"
+find "$source_dir/nonPorn/COCO/" -maxdepth 1 -type f -print0 | xargs -0 cp -t "$dest_dir"
+find "$source_dir/nonPorn/GVIS/" -maxdepth 1 -type f -print0 | xargs -0 cp -t "$dest_dir"
+find "$source_dir/nonPorn/ILSVRC2012/" -maxdepth 1 -type f -print0 | xargs -0 cp -t "$dest_dir"
+find "$source_dir/nonPorn/IMDB-WIKI/" -maxdepth 1 -type f -print0 | xargs -0 cp -t "$dest_dir"
 
 echo "Images moved successfully."
 
