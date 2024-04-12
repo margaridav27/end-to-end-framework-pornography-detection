76e3999e46db9c166e58ca8b24ecd6b89dd3a47e
Margarida Vieira <margarida.j.vieira@inesctec.pt>
2024-04-12 22:32:34 +0000

    case-based explanations

---
+++ scripts/case_based_xai.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH --partition=gpu_min24GB               
+#SBATCH --qos=gpu_min24GB_ext                 
+#SBATCH --job-name=case_based_xai
+#SBATCH -o case_based_xai.out               
+#SBATCH -e case_based_xai.err
+
+data_loc="/nas-ctm01/datasets/public/BIOMETRICS/pornography-2k-db/data-processed/even-20"
+save_loc="results/even-20/data-aug/pornography-2k/explanations"
+state_dict_loc="results/even-20/data-aug/pornography-2k/models"
+
+python -m src.case_based_xai \
+       --data_loc "$data_loc" \
+       --save_loc "$save_loc" \
+       --state_dict_loc "$state_dict_loc/vgg19_freeze_False_epochs_50_batch_16_optim_sgd_aug_True_split_10_20.pth"
