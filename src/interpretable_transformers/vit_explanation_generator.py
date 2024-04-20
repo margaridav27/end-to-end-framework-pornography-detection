0b9804a912f5c8910263c0d7e29a0f72ab08e40b
Margarida Vieira <margarida.j.vieira@inesctec.pt>
2024-04-20 12:52:54 +0000

    vit_explanation_generator functions annotations

--- src/interpretable_transformers/vit_explanation_generator.py
+++ src/interpretable_transformers/vit_explanation_generator.py
@@ -1,26 +1,29 @@
 # Source: https://github.com/hila-chefer/Transformer-Explainability
 
+from typing import List
+
 import numpy as np
 
 import torch
+import torch.nn as nn
 
 
 class LRP:
-    def __init__(self, model, device):
+    def __init__(self, model: nn.Module):
         self.model = model
         self.model.eval()
-        self.device = device
+        self.device = next(self.model.parameters()).device
 
     def generate_attribution(
         self,
         input_img,
         index=None,
-        method="transformer_attribution",
-        is_ablation=False,
-        start_layer=0,
-    ):
+        method: str = "transformer_attribution",
+        is_ablation: bool = False,
+        start_layer: int = 0,
+    ) -> torch.Tensor:
         output = self.model(input_img)
-        kwargs = {"alpha": 1}
+        kwargs = { "alpha": 1 }
 
         if index == None:
             index = np.argmax(output.cpu().data.numpy(), axis=-1)
@@ -29,7 +32,6 @@ class LRP:
         one_hot[0, index] = 1
         one_hot_vector = one_hot
         one_hot = torch.from_numpy(one_hot).requires_grad_(True)
-        # one_hot = torch.sum(one_hot.cuda() * output)
         one_hot = torch.sum(one_hot.to(self.device) * output)
 
         self.model.zero_grad()
@@ -45,19 +47,20 @@ class LRP:
 
 
 class Baselines:
-    def __init__(self, model):
+    def __init__(self, model: nn.Module):
         self.model = model
         self.model.eval()
+        self.device = next(self.model.parameters()).device
 
-    def generate_cam_attn(self, input, index=None):
-        output = self.model(input.cuda(), register_hook=True)
+    def generate_cam_attn(self, input: torch.Tensor, index=None):
+        output = self.model(input.to(self.device), register_hook=True)
         if index == None:
             index = np.argmax(output.cpu().data.numpy())
 
         one_hot = np.zeros((1, output.size()[-1]), dtype=np.float32)
         one_hot[0][index] = 1
         one_hot = torch.from_numpy(one_hot).requires_grad_(True)
-        one_hot = torch.sum(one_hot.cuda() * output)
+        one_hot = torch.sum(one_hot.to(self.device) * output)
 
         self.model.zero_grad()
         one_hot.backward(retain_graph=True)
@@ -72,39 +75,46 @@ class Baselines:
         cam = (cam - cam.min()) / (cam.max() - cam.min())
 
         return cam
-    
+
     @staticmethod
-    def compute_rollout_attention(all_layer_matrices, start_layer=0):
+    def compute_rollout_attention(
+        all_layer_matrices: List[torch.Tensor], 
+        start_layer: int = 0
+    ) -> torch.Tensor:
         batch_size = all_layer_matrices[0].shape[0]
         num_tokens = all_layer_matrices[0].shape[1]
+        
         eye = (
             torch.eye(num_tokens)
             .expand(batch_size, num_tokens, num_tokens)
             .to(all_layer_matrices[0].device)
         )
-        all_layer_matrices = [
-            all_layer_matrices[i] + eye for i in range(len(all_layer_matrices))
-        ]
+
+        all_layer_matrices = [all_layer_matrices[i] + eye for i in range(len(all_layer_matrices))]
         matrices_aug = [
             all_layer_matrices[i] / all_layer_matrices[i].sum(dim=-1, keepdim=True)
             for i in range(len(all_layer_matrices))
         ]
+        
         joint_attention = matrices_aug[start_layer]
         for i in range(start_layer + 1, len(matrices_aug)):
             joint_attention = matrices_aug[i].bmm(joint_attention)
 
         return joint_attention
 
-    def generate_rollout(self, input, start_layer=0):
+    def generate_rollout(self, input: torch.Tensor, start_layer: int = 0) -> torch.Tensor:
         self.model(input)
-        
+
         all_layer_attentions = []
         blocks = self.model.blocks
         for block in blocks:
             attn_heads = block.attn.attn_cam
             avg_heads = (attn_heads.sum(dim=1) / attn_heads.shape[1]).detach()
             all_layer_attentions.append(avg_heads)
-        
-        rollout = self.compute_rollout_attention(all_layer_matrices=all_layer_attentions, start_layer=start_layer)
-        
+
+        rollout = self.compute_rollout_attention(
+            all_layer_matrices=all_layer_attentions, 
+            start_layer=start_layer
+        )
+
         return rollout[:, 0, 1:]
