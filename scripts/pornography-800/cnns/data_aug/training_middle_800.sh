125064261e6e893f2be9153307182c9e3363c3ee
Margarida Vieira <margarida.j.vieira@inesctec.pt>
2024-05-13 09:56:35 +0000

    edit pornography-800 scripts

---
+++ scripts/pornography-800/cnns/data_aug/training_middle_800.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+#
+#SBATCH --partition=gpu_min24gb               
+#SBATCH --qos=gpu_min24gb_ext                 
+#SBATCH --job-name=training_aug_800
+#SBATCH -o training_aug_800.out               
+#SBATCH -e training_aug_800.err 
+
+
+project_title="training_aug_800"
+data_loc="/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/middle-20"
+results_loc="results/pornography-800/cnns/data-aug/middle-20"
+epochs=25
+batch_size=32
+
+
+echo "Running training job on Pornography-800 (middle-20, aug)"
+
+echo "Training ResNet50"
+python -m src.model_training \
+       --project_title "$project_title" \
+       --data_loc "$data_loc" \
+       --model_save_loc "$results_loc/models" \
+       --metrics_save_loc "$results_loc/metrics" \
+       --model_name "resnet50" \
+       --epochs $epochs \
+       --batch_size $batch_size \
+       --data_aug \
+       --wandb
+
+echo "Training ResNet101"
+python -m src.model_training \
+       --project_title "$project_title" \
+       --data_loc "$data_loc" \
+       --model_save_loc "$results_loc/models" \
+       --metrics_save_loc "$results_loc/metrics" \
+       --model_name "resnet101" \
+       --epochs $epochs \
+       --batch_size $batch_size \
+       --data_aug \
+       --wandb
+
+echo "Training ResNet152"
+python -m src.model_training \
+       --project_title "$project_title" \
+       --data_loc "$data_loc" \
+       --model_save_loc "$results_loc/models" \
+       --metrics_save_loc "$results_loc/metrics" \
+       --model_name "resnet152" \
+       --epochs $epochs \
+       --batch_size $batch_size \
+       --data_aug \
+       --wandb
+
+echo "Training DenseNet121"
+python -m src.model_training \
+       --project_title "$project_title" \
+       --data_loc "$data_loc" \
+       --model_save_loc "$results_loc/models" \
+       --metrics_save_loc "$results_loc/metrics" \
+       --model_name "densenet121" \
+       --epochs $epochs \
+       --batch_size $batch_size \
+       --data_aug \
+       --wandb
+
+echo "Training DenseNet169"
+python -m src.model_training \
+       --project_title "$project_title" \
+       --data_loc "$data_loc" \
+       --model_save_loc "$results_loc/models" \
+       --metrics_save_loc "$results_loc/metrics" \
+       --model_name "densenet169" \
+       --epochs $epochs \
+       --batch_size $batch_size \
+       --data_aug \
+       --wandb
+
+echo "Training DenseNet201"
+python -m src.model_training \
+       --project_title "$project_title" \
+       --data_loc "$data_loc" \
+       --model_save_loc "$results_loc/models" \
+       --metrics_save_loc "$results_loc/metrics" \
+       --model_name "densenet201" \
+       --epochs $epochs \
+       --batch_size $batch_size \
+       --data_aug \
+       --wandb
+
+echo "Training AlexNet"
+python -m src.model_training \
+       --project_title "$project_title" \
+       --data_loc "$data_loc" \
+       --model_save_loc "$results_loc/models" \
+       --metrics_save_loc "$results_loc/metrics" \
+       --model_name "alexnet" \
+       --epochs $epochs \
+       --batch_size $batch_size \
+       --data_aug \
+       --wandb
+
+echo "Training VGG16"
+python -m src.model_training \
+       --project_title "$project_title" \
+       --data_loc "$data_loc" \
+       --model_save_loc "$results_loc/models" \
+       --metrics_save_loc "$results_loc/metrics" \
+       --model_name "vgg16" \
+       --epochs $epochs \
+       --batch_size $batch_size \
+       --data_aug \
+       --wandb
+
+echo "Training VGG19"
+python -m src.model_training \
+       --project_title "$project_title" \
+       --data_loc "$data_loc" \
+       --model_save_loc "$results_loc/models" \
+       --metrics_save_loc "$results_loc/metrics" \
+       --model_name "vgg19" \
+       --epochs $epochs \
+       --batch_size $batch_size \
+       --data_aug \
+       --wandb
+
+echo "Training MobileNetV2"
+python -m src.model_training \
+       --project_title "$project_title" \
+       --data_loc "$data_loc" \
+       --model_save_loc "$results_loc/models" \
+       --metrics_save_loc "$results_loc/metrics" \
+       --model_name "mobilenet_v2" \
+       --epochs $epochs \
+       --batch_size $batch_size \
+       --data_aug \
+       --wandb
---
+++ scripts/pornography-800/cnns/no_freeze/testing_even_800.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+#SBATCH --partition=gpu_min8gb               
+#SBATCH --qos=gpu_min8gb_ext                 
+#SBATCH --job-name=testing_even_20_800
+#SBATCH -o testing_even_20_800.out               
+#SBATCH -e testing_even_20_800.err
+
+
+data_loc="/nas-ctm01/datasets/public/BIOMETRICS/pornography-database/data-processed/even-20"
+results_loc="results/pornography-800/cnns/no-freeze/even-20"
+
+
+echo "Running testing job on Pornography-800 (even-20)"
+
+echo "Testing ResNet50"
+python -m src.model_testing \
+       --data_loc "$data_loc" \
+       --save_loc "$results_loc/results" \
+       --state_dict_loc "$results_loc/models/resnet50_freeze_False_epochs_25_batch_32_optim_sgd_aug_False_split_10_20.pth"
+
+echo "Testing ResNet101"
+python -m src.model_testing \
+       --data_loc "$data_loc" \
+       --save_loc "$results_loc/results" \
+       --state_dict_loc "$results_loc/models/resnet101_freeze_False_epochs_25_batch_32_optim_sgd_aug_False_split_10_20.pth"
+
+echo "Testing ResNet152"
+python -m src.model_testing \
+       --data_loc "$data_loc" \
+       --save_loc "$results_loc/results" \
+       --state_dict_loc "$results_loc/models/resnet152_freeze_False_epochs_25_batch_32_optim_sgd_aug_False_split_10_20.pth"
+
+echo "Testing DenseNet121"
+python -m src.model_testing \
+       --data_loc "$data_loc" \
+       --save_loc "$results_loc/results" \
+       --state_dict_loc "$results_loc/models/densenet121_freeze_False_epochs_25_batch_32_optim_sgd_aug_False_split_10_20.pth"
+
+echo "Testing DenseNet169"
+python -m src.model_testing \
+       --data_loc "$data_loc" \
+       --save_loc "$results_loc/results" \
+       --state_dict_loc "$results_loc/models/densenet169_freeze_False_epochs_25_batch_32_optim_sgd_aug_False_split_10_20.pth"
+
+echo "Testing DenseNet201"
+python -m src.model_testing \
+       --data_loc "$data_loc" \
+       --save_loc "$results_loc/results" \
+       --state_dict_loc "$results_loc/models/densenet201_freeze_False_epochs_25_batch_32_optim_sgd_aug_False_split_10_20.pth"
+
+echo "Testing AlexNet"
+python -m src.model_testing \
+       --data_loc "$data_loc" \
+       --save_loc "$results_loc/results" \
+       --state_dict_loc "$results_loc/models/alexnet_freeze_False_epochs_25_batch_32_optim_sgd_aug_False_split_10_20.pth"
+
+echo "Testing VGG16"
+python -m src.model_testing \
+       --data_loc "$data_loc" \
+       --save_loc "$results_loc/results" \
+       --state_dict_loc "$results_loc/models/vgg16_freeze_False_epochs_25_batch_32_optim_sgd_aug_False_split_10_20.pth"
+
+echo "Testing VGG19"
+python -m src.model_testing \
+       --data_loc "$data_loc" \
+       --save_loc "$results_loc/results" \
+       --state_dict_loc "$results_loc/models/vgg19_freeze_False_epochs_25_batch_32_optim_sgd_aug_False_split_10_20.pth"
+
+echo "Testing MobileNetV2"
+python -m src.model_testing \
+       --data_loc "$data_loc" \
+       --save_loc "$results_loc/results" \
+       --state_dict_loc "$results_loc/models/mobilenet_v2_freeze_False_epochs_25_batch_32_optim_sgd_aug_False_split_10_20.pth"
