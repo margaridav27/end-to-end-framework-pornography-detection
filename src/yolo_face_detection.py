e5167ce9a54edff54cc3d7f47f033290ab69027a
Margarida Vieira <margarida.j.vieira@inesctec.pt>
2024-06-16 21:52:58 +0000

    yolo face detection

--- src/yolo_face_detection.py
+++ src/yolo_face_detection.py
@@ -1,14 +1,12 @@
 from src.utils.misc import set_device
-from src.utils.data import load_split
-from src.datasets.pornography_frame_dataset import PornographyFrameDataset
 
 import os
 import argparse
 import json
+import cv2
 import albumentations as A
 
 import torch
-from torch.utils.data import DataLoader
 
 from ultralytics import YOLO
 
@@ -16,58 +14,69 @@ from ultralytics import YOLO
 def _parse_arguments():
     parser = argparse.ArgumentParser(description="YOLO face detection")
     parser.add_argument("--data_loc", type=str, required=True)
-    parser.add_argument("--save_loc", type=str, required=True, help="Directory to save the results")
+    parser.add_argument("--save_loc", type=str, help="Directory to save the results")
     parser.add_argument("--weights", type=str, default="yolov8m-face.pt")
     parser.add_argument("--conf_thres", type=float, default=0.6, help="Object confidence threshold")
+    parser.add_argument("--input_shape", type=int)
     parser.add_argument("--batch_size", type=int, default=32)
-    parser.add_argument("--split", type=float, nargs="*", default=[0.1, 0.2], help="Validation and test")
-    parser.add_argument("--input_shape", type=int, default=224)
+    parser.add_argument("--to_detect", type=str, nargs="*")
+    parser.add_argument("--show", action="store_true", default=False)
+    parser.add_argument("--save", action="store_true", default=False)
 
     args = parser.parse_args()
 
     if not os.path.exists(args.data_loc):
         parser.error("Invalid --data_loc argument.")
 
-    os.makedirs(args.save_loc, exist_ok=True)
+    if args.save:
+        if args.save_loc is None:
+            parser.error("You must specify --save_loc argument when --save is True.")
+        else:
+            os.makedirs(args.save_loc, exist_ok=True)
 
     return args
 
 
 def main():
-    args = _parse_arguments() 
+    args = _parse_arguments()
+
+    if not (args.show or args.save): return 
 
     device = set_device()
 
-    h_transf = w_transf = args.input_shape
-    data_transforms = A.Resize(height=h_transf, width=w_transf)
-
-    df_test = load_split(args.data_loc, args.split, "test")["test"]
-    dataset = PornographyFrameDataset(args.data_loc, df_test, data_transforms)
-    dataloader = DataLoader(dataset=dataset, batch_size=args.batch_size)
-
     model = YOLO(args.weights)
 
-    for names, _, _, orig_shapes in dataloader:        
-        results = model.predict(
-            source=[f"{args.data_loc}/{name}" for name in names],
+    to_detect = (
+        args.to_detect
+        if args.to_detect is not None and len(args.to_detect) > 0
+        else os.listdir(args.data_loc)
+    )
+
+    if args.show and args.input_shape is not None:
+        data_transforms = A.Resize(height=args.input_shape, width=args.input_shape)
+
+    for filename in to_detect:
+        result = model.predict(
+            source=f"{args.data_loc}/{filename}",
             conf=args.conf_thres,
             device=device,
-        )
+        )[0].cpu()
 
-        for i, result in enumerate(results):
-            result = result.cpu()
+        n_detected = result.boxes.data.size()[0]
+        print(f"{filename}: {n_detected} faces detected")
 
-            n_detected = result.boxes.data.size()[0]
-            print(f"{names[i]}: {n_detected} faces detected")
+        # Check if any bounding boxes were detected
+        if n_detected == 0: continue
 
-            # check if any bounding boxes were detected
-            if n_detected == 0: continue
+        # Discard the keypoints
+        result.keypoints = None
 
-            # discard the keypoints
-            result.keypoints = None
+        # Map bounding box coordinates to given input shape
+        if args.input_shape is not None:
+            image = cv2.imread(os.path.join(args.data_loc, filename))
 
-            h_orig, w_orig, _ = orig_shapes[i]
-            h_ratio, w_ratio = h_transf / h_orig, w_transf / w_orig
+            h_orig, w_orig, _ = image.shape
+            h_ratio, w_ratio = args.input_shape / h_orig, args.input_shape / w_orig
 
             # adjust bounding boxes coordinates
             # had to do this because the performance was worse if detecting on the resized images
@@ -79,8 +88,17 @@ def main():
 
             result.update(boxes=adjusted_boxes)
 
-            with open(f"{args.save_loc}/{os.path.splitext(names[i])[0]}.json", "w") as file:
+            # No need to transform the image if not showing the result
+            if args.show:
+                image = data_transforms(image=image)["image"]
+                result.orig_img = image
+
+        if args.save:
+            with open(f"{args.save_loc}/{os.path.splitext(filename)[0]}.json", "w") as file:
                 json.dump(json.loads(result.tojson()), file)
+
+        if args.show:
+            result.show()
 
 
 if __name__ == "__main__":
